bf485c015b03c800ad0fc9caf9c4e8e16986ad23
Nattawat Lertchidomrongsri <Nattawat.lert@dome.tu.ac.th>
2026-01-17 18:45:10 +0700

    Add toString() method

---
+++ Java/Javaoop/A9toStringMethod/Main.java
@@ -0,0 +1,18 @@
+package Javaoop.A9toStringMethod;
+
+public class Main {
+    public static void main(String[] args){
+
+        // .toString() = Method inherited from the object class.
+        //               Used to return a string repersenttation of an object.
+        //               By default, it returns a hash code as a unique identifier.
+        //               It can be overrindden to provide meaningful details.
+    
+        Car car1 = new Car("Ford", "Mustang", 2025, "Red");
+        Car car2 = new Car("Chevrolet", "Corvette", 2026, "Blue");
+
+        System.out.println(car1); // Red 2025 Ford Mustang
+        System.out.println(car2); // Blue 2026 Chevrolet Corvette
+    
+    }
+}
