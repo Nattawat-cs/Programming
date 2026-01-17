ab68622d330e49f32c78b7bda4a0d28ccd241e0b
Nattawat Lertchidomrongsri <Nattawat.lert@dome.tu.ac.th>
2026-01-17 08:55:48 +0700

    Change L02

--- Java/L02variables.java
+++ Java/L02variables.java
@@ -23,21 +23,18 @@ public class L02variables {
     //Ex.1 int
     int age = 21;
     int year = 2025;
-    int quantity = 1;
+    
 
     System.out.println(age + " years old");
     System.out.println("The year is " + year);
 
     //Ex.2 double
     double price = 19.99;
-    double gpa = 3.5;
-    double temperature = 12.5;
     
     System.out.println(price);
 
     //Ex.3 char
     char grade = 'A';
-    char symbol = '!';
     char currency = '$';
 
     System.out.println(grade);
@@ -45,7 +42,7 @@ public class L02variables {
     //Ex.4 boolean
     boolean isStudent = true;
     boolean forSale = false;
-    boolean isOnline = true;
+    
 
     System.out.println(isStudent);
     
