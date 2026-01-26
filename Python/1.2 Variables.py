3c04523841777fe540f5f18ff26e9fa59878d388
Nattawat Lertchidumrongsri <little0747@gmail.com>
2026-01-26 17:33:42 +0700

    Add Intro and Variable

---
+++ Python/1.2 Variables.py
@@ -0,0 +1,29 @@
+# Variable = A container for a value (String, integer, float, boolean)
+#            A variable behaves as if it was the value it contains
+
+#String
+first_name = "Bro"
+food = "pizza"
+email = "Bro123@fake.com"
+
+print(f"Hello {first_name}")
+print(f"You like {pizza}")
+print(f"Your email is: {email}")
+
+#Integers
+age = 25
+quantity = 3
+num_of_student = 30
+
+print(f"You are {age} years old")
+print(f"You are buying {quantity} item")
+print(f"You class has {num_of_student} student")
+
+#Float
+price = 10.99
+gpa = 3.2
+distance = 5.5
+
+print(f"the price is ${price}")
+print(f"Your gpa is: {gpa}")
+print(f"You ran {distance}")
