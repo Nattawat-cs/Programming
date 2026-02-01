c6c4d1bc3d7efb2500706b551157de1abb9ce0e7
Nattawat Lertchidumrongsri-6809617126 <little0747@gmail.com>
2026-02-01 07:45:43 +0000

    Add exceptionhandling

---
+++ Java/L28exceptionhandling.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+public class L28exceptionhandling {
+    public static void main(String[] args) {
+
+        // Exception = An event that interruptions the normal flow of a program
+        //             (Dividing by zero, file not found, mismatch input type)
+        //             Surround any dangerous code with a try{} block
+        //             try{}, catch{}, finally{}
+
+        try (Scanner scanner = new Scanner(System.in)){
+            System.out.println("Enter a number");
+            int number = scanner.nextInt();
+            System.out.println(number);
+        }
+        catch(InputMismatchException e){
+            System.out.println("That wasn't a number!");
+        }
+        catch(ArithmeticException e){
+            System.out.println("YOU CAN'T DIVIDE BY ZERO!");
+        }
+        catch(Exception e){
+            // SAFETY NET
+            System.out.println("Something went wrong");
+        }
+        finally{
+            System.out.println("This always executes");
+        }
+    }
+}
